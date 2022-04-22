1010f3cdedefe18eee4361966621e0de8aa7db3f
Dario Benevento <dario.benevento@georgfischer.com>
2022-04-22 11:23:32 +0200

    Added LocalizerManager DRAFT

---
+++ BlazorLocalization/BlazorLocalization/Shared/ResourceManagerStringLocalizerFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace BlazorLocalization.Shared
+{
+    internal class ResourceManagerStringLocalizerFactory : IStringLocalizerFactory
+    {
+        private IStringLocalizerFactory _stringLocalizerFactory;
+
+        public IStringLocalizer Create(Type resourceSource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStringLocalizer Create(string baseName, string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResourceManagerStringLocalizerFactory(IStringLocalizerFactory stringLocalizerFactory)
+        {
+            _stringLocalizerFactory = stringLocalizerFactory;
+        }
+    }
+}
--- BlazorLocalization/BlazorLocalization/Shared/WeatherForecast.cs
+++ BlazorLocalization/BlazorLocalization/Shared/WeatherForecast.cs
@@ -4,11 +4,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlazorLocalization.Shared.Languages;
+using Microsoft.Extensions.Localization;
+
 
 namespace BlazorLocalization.Shared
 {
     public class WeatherForecast
     {
+        private IStringLocalizer<ResourceLanguage> localizer;
+
         public DateTime Date { get; set; }
 
         [Range(18, 80, ErrorMessage = "Temperature must be between 18°C and 22°C.")]
@@ -18,5 +23,11 @@ namespace BlazorLocalization.Shared
         public string? Summary { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public WeatherForecast()
+        {
+            localizer = new StringLocalizer<ResourceLanguage>(new ResourceManagerStringLocalizerFactory(this));
+
+        }
     }
 }
